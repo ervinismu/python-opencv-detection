126e46c0fac4fc7a4ca46325be96518111da6e47
ervinismu <social.ervin@gmail.com>
2018-10-21 23:14:57 +0700

    Add image wrtie

--- image_capturing.py
+++ image_capturing.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 # create object zero for external camera
 video = cv2.VideoCapture(0)
@@ -8,32 +9,38 @@ a = 0
 
 while True:
     a = a + 1
-
+    
     # frame object
     check, frame = video.read()
-
+    
+    # showing check and frame in terminal log
     print(check)
     print(frame)
-
+    
     # converting to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    
+    # please replace this variable with your folder path for save image in folder
+    path = None
+    
     # show frame
     cv2.imshow("captureing", gray)
-
-    # for press any key to out (miliseconds)
-    #cv2.waitKey(0)
+    
+    if path is None:
+        cv2.imwrite('image_{}.jpg'.format(a), frame)
+    else:
+        cv2.imwrite(os.path.join(path , 'image_{}.jpg'.format(a)), frame)
 
     # for playing
     key = cv2.waitKey(2)
-
+    
+    # key for close capturing video
     if key == ord('q'):
         break
 
 # show how many milisecond the stream will take
-print(a)
+print('number of photos: ', a)
 
 # shutdown the camera
 video.release()
-
 cv2.destroyAllWindows
